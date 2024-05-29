51e8873ae2b92ccfbb82e47005b316b3293f0773
robsu17 <robson.costa134@gmail.com>
2024-05-29 08:59:28 -0300

    update check-in-repository

--- src/repositories/check-in-repository.ts
+++ src/repositories/check-in-repository.ts
@@ -1,6 +1,7 @@
 import { CheckIn, Prisma } from '@prisma/client'
 
 export interface ICheckInsRepository {
+  items: CheckIn[]
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   countByUserId(userId: string): Promise<number>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
