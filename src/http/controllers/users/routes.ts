4b11bd603a7d286bf056a6f3d2ca02758fa45326
robsu17 <robson.costa134@gmail.com>
2024-06-13 09:09:02 -0300

    feat: rota de refresh token implementado

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { profile } from './profile'
 import { register } from './register'
 import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '../../middleware/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
