37b5b5263050e862814afcd64504e72d5e838bb1
robsu17 <robson.costa134@gmail.com>
2025-02-25 06:23:34 -0300

    add line

--- src/utils/get-distance-between-coordinates.ts
+++ src/utils/get-distance-between-coordinates.ts
@@ -7,6 +7,7 @@ export function getDistanceBetweenCoordinates(
   from: Coordinate,
   to: Coordinate,
 ) {
+  
   if (from.latitude === to.latitude && from.longitude === to.longitude) {
     return 0
   }
